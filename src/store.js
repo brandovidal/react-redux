c36a7837881dde88691c1a6d85fbde56c414fefb
Brando Vidal <yisusdracon@gmail.com>
2021-11-20 18:28:13 -0500

    feat: implement react thunk in middleware

--- src/store.js
+++ src/store.js
@@ -1,12 +1,13 @@
 import { noteReducer } from './reducer/noteReducer'
 import { filterReducer } from './reducer/filterReducer'
 
-import { combineReducers, createStore } from 'redux'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
 
 const reducer = combineReducers({
   notes: noteReducer,
   filter: filterReducer
 })
 
-export const store = createStore(reducer, composeWithDevTools())
+export const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
