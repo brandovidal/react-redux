fd809f2895d044f2aeb525074cbd41b044d4a8af
Brando Vidal <yisusdracon@gmail.com>
2021-11-20 04:39:53 -0500

    feat: create service

---
+++ src/services/notes.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/notes'
+
+export const getAll = async () => {
+  const res = await axios.get(baseUrl)
+  return res.data
+}
+
+export const createNew = async (content) => {
+  const note = { content, importanat: false }
+  const response = await axios.post(baseUrl, note)
+  return response.data
+}
